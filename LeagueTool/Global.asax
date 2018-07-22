6d9ae28917443d826883298d65b5d09889dd6f0d
jlefever <jlefever@ycp.edu>
2018-07-21 20:20:04 -0400

    Install and configure Autofac for dependency injection

--- LeagueTool/Global.asax
+++ LeagueTool/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeagueTool.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeagueTool.LeagueTool" Language="C#" %>
--- LeagueTool/Global.asax.cs
+++ LeagueTool/Global.asax.cs
@@ -1,18 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using LeagueTool.Services;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace LeagueTool
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class LeagueTool : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ConfigureIoc();
+        }
+
+        private void ConfigureIoc()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(LeagueTool).Assembly);
+
+            builder.RegisterType<ConfigService>().AsSelf().SingleInstance();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
