6f130daa296f2b8b3299f9590e7493579d3eaffc
jlefever <jlefever@ycp.edu>
2018-07-21 20:00:58 -0400

    Add ConfigService to load app settings as static types

---
+++ LeagueTool/Services/ConfigService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Configuration;
+
+namespace LeagueTool.Services
+{
+    public class ConfigService
+    {
+        public bool DummySetting { get; }
+
+        public ConfigService()
+        {
+            DummySetting = GetAppSetting<bool>("DummySetting");
+        }
+
+        private T GetAppSetting<T>(string key)
+        {
+            var value = WebConfigurationManager.AppSettings[key];
+            return (T)Convert.ChangeType(value, typeof(T));
+        }
+    }
+}
