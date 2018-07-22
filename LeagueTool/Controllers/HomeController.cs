38c72b79975a690de1de35f1e45091f356b9d9bc
jlefever <jlefever@ycp.edu>
2018-07-21 20:21:56 -0400

    Add initial HomeController and view

---
+++ LeagueTool/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using LeagueTool.Services;
+using System.Web.Mvc;
+
+namespace LeagueTool.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ConfigService _configService;
+
+        public HomeController(ConfigService configService)
+        {
+            _configService = configService;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.DummySetting = _configService.DummySetting;
+            return View();
+        }
+    }
+}
