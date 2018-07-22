4235732231a87b3e1584b0a8734ead8f5f0ec40e
jlefever <jlefever@ycp.edu>
2018-07-22 01:29:33 -0400

    Install and configure MediatR

--- LeagueTool/Global.asax.cs
+++ LeagueTool/Global.asax.cs
@@ -1,11 +1,14 @@
-﻿using Autofac;
+﻿using System.Net.NetworkInformation;
+using System.Reflection;
+using Autofac;
 using Autofac.Integration.Mvc;
 using LeagueTool.Services;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using LeagueTool.Factories;
-using RiotSharp.Interfaces;
+using MediatR;
+using MediatR.Pipeline;
 
 namespace LeagueTool
 {
@@ -28,8 +31,35 @@ namespace LeagueTool
             builder.Register(c => c.Resolve<RiotApiFactory>().CreateRiotApi()).AsSelf().SingleInstance();
             builder.Register(c => c.Resolve<RiotApiFactory>().CreateStaticRiotApi()).AsSelf().SingleInstance();
 
+            ConfigureMediatr(builder);
+
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private static void ConfigureMediatr(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(IMediator).GetTypeInfo().Assembly).AsImplementedInterfaces();
+
+            var mediatrOpenTypes = new[]
+            {
+                typeof(IRequestHandler<,>),
+                typeof(INotificationHandler<>)
+            };
+
+            foreach (var mediatrOpenType in mediatrOpenTypes)
+            {
+                builder
+                    .RegisterAssemblyTypes(typeof(LeagueTool).GetTypeInfo().Assembly)
+                    .AsClosedTypesOf(mediatrOpenType)
+                    .AsImplementedInterfaces();
+            }
+
+            builder.Register<ServiceFactory>(ctx =>
+            {
+                var c = ctx.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
         }
     }
 }
