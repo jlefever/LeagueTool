d421e30d34e297cdc6d012c5e6fbe10275b579a1
jlefever <jlefever@ycp.edu>
2018-08-03 14:34:34 -0400

    Update Region.cs to include DisplayName

--- LeagueTool/Models/Region.cs
+++ LeagueTool/Models/Region.cs
@@ -5,42 +5,44 @@ namespace LeagueTool.Models
 {
     public class Region
     {
-        public static readonly Region Br = new Region("br");
-        public static readonly Region Eune = new Region("eune");
-        public static readonly Region Euw = new Region("euw");
-        public static readonly Region Jp = new Region("jp");
-        public static readonly Region Kr = new Region("kr");
-        public static readonly Region Lan = new Region("lan");
-        public static readonly Region Las = new Region("las");
-        public static readonly Region Na = new Region("na");
-        public static readonly Region Oce = new Region("oce");
-        public static readonly Region Tr = new Region("tr");
-        public static readonly Region Ru = new Region("ru");
-        public static readonly Region Pbe = new Region("pbe");
+        public static readonly Region Na = new Region("na", "North America");
+        public static readonly Region Euw = new Region("euw", "Europe West");
+        public static readonly Region Eune = new Region("eune", "Europe Nordic & East");
+        public static readonly Region Lan = new Region("lan", "Latin America North");
+        public static readonly Region Las = new Region("las", "Latin America South");
+        public static readonly Region Br = new Region("br", "Brazil");
+        public static readonly Region Tr = new Region("tr", "Turkey");
+        public static readonly Region Ru = new Region("ru", "Russia");
+        public static readonly Region Oce = new Region("oce", "Oceania");
+        public static readonly Region Jp = new Region("jp", "Japan");
+        public static readonly Region Kr = new Region("kr", "Korea");
+        public static readonly Region Pbe = new Region("pbe", "Public Beta Environment");
 
         public string Name { get; }
+        public string DisplayName { get; }
 
-        private Region(string name)
+        private Region(string name, string displayName)
         {
             Name = name;
+            DisplayName = displayName;
         }
 
         public static IEnumerable<Region> All()
         {
             return new[]
             {
-                Br,
-                Eune,
+                Na,
                 Euw,
-                Jp,
-                Kr,
+                Eune,
                 Lan,
                 Las,
-                Na,
-                Oce,
+                Br,
                 Tr,
                 Ru,
-                Pbe
+                Oce,
+                Jp,
+                Kr,
+                Pbe,
             };
         }
 
