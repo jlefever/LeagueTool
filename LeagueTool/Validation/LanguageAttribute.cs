c02a49a5afb80972d03fe12254e62ba44fde411a
jlefever <jlefever@ycp.edu>
2018-08-06 19:45:32 -0400

    Add form validation for Region, Language, and Version

---
+++ LeagueTool/Validation/LanguageAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using LeagueTool.Models;
+
+namespace LeagueTool.Validation
+{
+    public class LanguageAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (!(value is string language))
+            {
+                return new ValidationResult("Languages must be a string.");
+            }
+
+            if (!Language.IsValidLanguage(language))
+            {
+                return new ValidationResult("Not a valid language.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
