c29f141752314c10a36293ce9ad5ee2a04b9792a
jlefever <jlefever@ycp.edu>
2018-08-07 14:00:10 -0400

    Fix bug where Spanish language would crash the champion list

--- LeagueTool/Models/ViewModels/ChampionListItemModel.cs
+++ LeagueTool/Models/ViewModels/ChampionListItemModel.cs
@@ -2,26 +2,8 @@
 {
     public class ChampionListItemModel
     {
-        private string _title;
-
-        public string Title
-        {
-            get => _title;
-
-            set
-            {
-                if (value[0].Equals('t'))
-                {
-                    _title = "T" + value.Substring(1);
-                }
-                else
-                {
-                    _title = value;
-                }
-            }
-        }
-
         public int Id { get; set; }
+        public string Title { get; set; }
         public string Name { get; set; }
         public string UrlName { get; set; }
         public string SquareImage { get; set; }
